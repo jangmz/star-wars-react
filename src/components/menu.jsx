a0e0037671a49d5f1c98cfd65008ed48c8e88836
Jan <131482887+jangmz@users.noreply.github.com>
2024-06-14 13:33:02 +0200

    styled navigation menu

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,10 +1,10 @@
 export default function Menu() {
     return (
-        <div>
+        <div className="navigation-menu">
             <ul>
                 <li>Upcoming</li>
                 <li>Legacy</li>
-                <li><img src="#" alt=""/></li>
+                <li><img src="../images/starwars.png" alt="star wars title"/></li>
                 <li>Merch</li>
                 <li>About</li>
             </ul>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import "./styles/App.css";
+import "./styles/NavigationMenu.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
