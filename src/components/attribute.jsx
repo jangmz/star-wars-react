2b8cc60b7041c41cb2e45bb941cc8601f18ab381
Jan <131482887+jangmz@users.noreply.github.com>
2024-06-16 10:27:58 +0200

    added metric units cm/kg

--- src/components/attribute.jsx
+++ src/components/attribute.jsx
@@ -2,7 +2,8 @@
 export default function Attribute({ attrib, value}) {
     return (
         <li key={attrib}>
-            {attrib[0].toUpperCase() + attrib.substring(1)}: <strong>{value}</strong>
+            {attrib[0].toUpperCase() + attrib.substring(1)}: <strong>{value}</strong> 
+            {attrib === "height" ? <strong>&nbsp;cm</strong> : attrib === "mass" ? <strong>&nbsp;kg</strong> : <strong></strong>}
         </li>
     )
 }
